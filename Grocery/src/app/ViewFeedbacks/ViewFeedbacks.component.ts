335ab7f313e8d97f6c1ed5864062fc7ccea07565
kalaivani <kalaivani0720@gmail.com>
2023-12-28 21:24:00 +0530

    addnew

--- Grocery/src/app/ViewFeedbacks/ViewFeedbacks.component.ts
+++ Grocery/src/app/ViewFeedbacks/ViewFeedbacks.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FeedbackserviceService } from '../feedbackservice.service';
+import Swal from 'sweetalert2';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-ViewFeedbacks',
@@ -8,14 +10,36 @@ import { FeedbackserviceService } from '../feedbackservice.service';
 })
 export class ViewFeedbacksComponent implements OnInit {
 
-  constructor(private feedback:FeedbackserviceService) { }
+  constructor(private feedback:FeedbackserviceService,private form:FormBuilder) { }
 
   OrderFeedback:any;
   ProductFeedback:any;
   show:any;
+  query:any;
+  replyform:any=this.form.group({
+   reply:[]
+  });
+  queryid:any;
+
   ngOnInit() {
     this.getdata();
   }
+  update(id:any)
+  {
+     this.queryid=id;
+  }
+updatereply(reply:any)
+{
+  console.log(reply.reply);
+  let body=[{op:"replace",path:"/reply", value:reply.reply}];
+  this.feedback.updatequery(this.queryid,body).subscribe((res:any)=>{
+    Swal.fire({
+      title:'success',
+      text:'reply updated!!!',
+    });
+  })
+
+}
 getdata()
 {
   this.show="Order";
@@ -33,8 +57,8 @@ getproductfeedback()
 getquery()
 {
   this.show="Queries";
-  this.feedback.getOrderFeedback().subscribe((res:any)=>{
-    this.OrderFeedback=res;
+  this.feedback.getquery().subscribe((res:any)=>{
+    this.query=res;
   })
 }
 }
